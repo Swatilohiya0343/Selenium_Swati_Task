6077b0b4976e073935f90028600798677d4d5e81
Swatilohiya0343 <161947524+Swatilohiya0343@users.noreply.github.com>
2024-03-03 11:15:25 +0530

    Update Selenium_task.py

--- Selenium_task.py
+++ Selenium_task.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from time import sleep
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.common.keys import Keys
 
 from selenium.common.exceptions  import NoSuchElementException
+count = 1
+chrome_driver_path = "msedgedriver.exe"
+browser_path = "C:\\Program Files (x86)\\Microsoft\\Edge Beta\\Application\\msedge.exe" 
+option = EdgeOptions()
+option.use_chromium = True  
+option.binary_location = browser_path   
+driver= Edge(executable_path = chrome_driver_path, options = option)
 url="https://www.cbse.gov.in/cbsenew/question-paper.html"
 
-webdriver_path = "msedgedriver.exe"
 
-# Initialize Edge WebDriver with the full path to the WebDriver executable
-browser = webdriver.Edge(webdriver_path) 
-browser.get(url)
+driver.get(url)
 for i in range(1,11):
     try:
         page1=browser.find_element_by_xpath('//*[@id="cbp-ntaccordion"]/li['+str(i)+']/h3')
